6bfb54d5fdd6dbc3f320aae68295a93f3f0b3a01
ThippayawadeeS <Thippayawadees@gmail.com>
2021-02-28 16:59:45 +0700

    initial commit

---
+++ src/app/task-item/task-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Task } from '../task';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskComponent implements OnInit {
+  @Input()
+  product: Task;
+
+  @Output()
+  OnSelected: EventEmitter<TaskComponent> = new EventEmitter();
+  
+  isSelected: boolean;
+
+  constructor() {}
+
+  ngOnInit () {}
+
+  select() {
+     this.OnSelected.emit(this); 
+     
+  }
+} 
